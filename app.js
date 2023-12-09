1d2d36d817b2087eddc0abbf1af820873481bddc
Sophia Mae San Agustin <smsanagustin@up.edu.ph>
2023-12-09 20:24:46 +0800

    added post handler

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.listen(3000, () => {
     console.log('Started listening to port 3000');
 })
 
+app.use(express.urlencoded( { extended: true }));
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
 })
@@ -16,6 +18,10 @@ app.get("/about", (req, res) => {
 
 app.get("/blogs", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/blogs.html'))
+})
+
+app.post('/blogs', (req, res) => {
+    console.log(req.body)
 })
 
 app.get("/create", (req, res) => {
